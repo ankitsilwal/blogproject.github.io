2347832cf95b0bb87fc89d42154e16e424065d14
Ankit Kumar <108688582+ankitsilwal@users.noreply.github.com>
2024-01-16 13:12:20 +0530

    remove-extra

--- src/AuthModule/RolesGuard/role.decorator.ts
+++
@@ -1,3 +0,0 @@
-import { SetMetadata } from "@nestjs/common/decorators/core/set-metadata.decorator";
-import { UserRole } from "src/AuthModule/Dto/createUserDto";
-export const UserRoles = (...roles: UserRole[]) => SetMetadata("roles", roles);
